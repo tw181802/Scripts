b3727ddebb2a3b2178ae625ae739544ee547c37d
Trent <106920505+tw181802@users.noreply.github.com>
2024-09-13 19:04:07 -0400

    Update add.py

--- python/add.py
+++ python/add.py
@@ -1,4 +1,31 @@
+#Arithmetic
 a = 5
 b = 10
 sum = a + b
 print(sum)
+
+#Functions
+def add(a, b):
+    return a + b
+print(add(5, 10))
+
+
+#Conditionals If-Else
+number = 10
+if number > 5:
+    print("Greater than 5")
+else:
+    print("5 or less")
+
+
+#Loops
+for i in range(5):
+    print(i)
+
+
+
+#Data Structures (Arrays & Lists)
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print(number)
+
