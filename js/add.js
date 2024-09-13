188d1480a11b1ff62987325bb104da18fb2dfdaf
Trent <106920505+tw181802@users.noreply.github.com>
2024-09-13 18:56:13 -0400

    Update add.js

--- js/add.js
+++ js/add.js
@@ -1,4 +1,29 @@
+//Arithmetic
 let a = 5;
 let b = 10;
 let sum = a + b;
 console.log(sum);
+
+//Functions
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 10));
+
+//Conditionals If-Else
+let number = 10;
+if (number > 5) {
+    console.log("Greater than 5");
+} else {
+    console.log("5 or less");
+}
+
+//Loops
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+//Data Structures (Arrays/Lists)
+let numbers = [1, 2, 3, 4, 5];
+numbers.forEach(number => console.log(number));
+
