1d9a082172e37fd93d21c1c866ffe928255afc54
Trent <106920505+tw181802@users.noreply.github.com>
2024-09-13 18:42:46 -0400

    Update add.sh

--- bash/add.sh
+++ bash/add.sh
@@ -1,4 +1,28 @@
+# Arithmetic
 a=5
 b=10
 sum=$((a + b))
 echo $sum
+
+#Functions
+add() {
+    local a=$1
+    local b=$2
+    echo $((a + b))
+}
+
+add 5 10
+
+#Conditionals if-else
+number=10
+if [ $number -gt 5 ]; then
+    echo "Greater than 5"
+else
+    echo "5 or less"
+fi
+
+# Loops
+for i in {0..4}; do
+    echo $i
+done
+
