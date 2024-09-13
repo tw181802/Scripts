6d41695aacec908ca760d9bca0cc143d977319cb
Trent <106920505+tw181802@users.noreply.github.com>
2024-09-13 18:52:33 -0400

    Update add.cs

--- cs/add.cs
+++ cs/add.cs
@@ -1,4 +1,37 @@
+//Arithmetic
 int a = 5;
 int b = 10;
 int sum = a + b;
 Console.WriteLine(sum);
+
+//Functions
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+Console.WriteLine(Add(5, 10));
+
+// Conditionals If-else
+int number = 10;
+if (number > 5)
+{
+    Console.WriteLine("Greater than 5");
+}
+else
+{
+    Console.WriteLine("5 or less");
+}
+
+// Loops
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+}
+
+//Data Structures (Arrays/List)
+int[] numbers = { 1, 2, 3, 4, 5 };
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
