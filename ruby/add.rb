3fdbd6f8dc520cfefe7c8efd4e6c262aa6799016
Trent <106920505+tw181802@users.noreply.github.com>
2024-09-13 19:06:40 -0400

    Update add.rb

--- ruby/add.rb
+++ ruby/add.rb
@@ -1,4 +1,28 @@
+# Arithmetic
 a = 5
 b = 10
 sum = a + b
 puts sum
+
+#Functions
+def add(a, b)
+  a + b
+end
+
+puts add(5, 10)
+
+
+#Conditionals If-Else
+number = 10
+if number > 5
+  puts "Greater than 5"
+else
+  puts "5 or less"
+end
+
+
+#Loops
+for i in 0..4
+  puts i
+end
+
