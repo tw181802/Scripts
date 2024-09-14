b473a9c017cd3663533b2957c9656d3f65ea2324
Trent <106920505+tw181802@users.noreply.github.com>
2024-09-14 08:58:48 -0400

    Update add.cpp

--- cpp/add.cpp
+++ cpp/add.cpp
@@ -1,40 +1,67 @@
+//I added an #include <iostream> directive to import the iostream header, which provides input/output functionality, including std::cout.
+//I added a main function, which is the entry point for the program. This is where the program starts execution.
+//I added a return 0 statement at the end of the main function to indicate successful execution.
+
 //Arithmetic
-int a = 5;
-int b = 10;
-int sum = a + b;
-std::cout << sum << std::endl;
+#include <iostream>
+int main() {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    std::cout << sum << std::endl;
+    return 0;
+}
 
 
 // Functions
+#include <iostream>
+
 int Add(int a, int b)
 {
     return a + b;
 }
 
-std::cout << Add(5, 10) << std::endl;
-
-//Conditionals if-else
-int number = 10;
-if (number > 5)
-{
-    std::cout << "Greater than 5" << std::endl;
+int main() {
+    std::cout << Add(5, 10) << std::endl;
+    return 0;
 }
-else
-{
-    std::cout << "5 or less" << std::endl;
+//Conditionals if-else
+#include <iostream>
+
+int main() {
+    int number = 10;
+    if (number > 5)
+    {
+        std::cout << "Greater than 5" << std::endl;
+    }
+    else
+    {
+        std::cout << "5 or less" << std::endl;
+    }
+    return 0;
 }
 
 
 // Loops
-for (int i = 0; i < 5; i++)
-{
-    std::cout << i << std::endl;
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << i << std::endl;
+    }
+    return 0;
 }
 
 
 //Data Structues (arrays/lists)
-int numbers[] = {1, 2, 3, 4, 5};
-for (int i = 0; i < 5; i++)
-{
-    std::cout << numbers[i] << std::endl;
+#include <iostream>
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << numbers[i] << std::endl;
+    }
+    return 0;
 }
